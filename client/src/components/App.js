5d76fc217d2358c3873eb23a1b322b5b9636c944
chalana kalpitha <chalanakalpitha72@gmail.com>
2023-01-29 10:48:33 +0530

    route setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,26 @@
 import "../styles/App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Root element</div>,
+  },
+  {
+    path: "/quiz",
+    element: <div>Quiz element</div>,
+  },
+  {
+    path: "/result",
+    element: <div>Result element</div>,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
